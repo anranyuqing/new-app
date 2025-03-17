04b32f93ccba2360f694d238aaa7a2191e6437b8
anranyuqing <483498230@qq.com>
2025-03-17 16:07:32 +0800

    完善顶部操作栏

--- src/layout/components/HeaderLayout/index.vue
+++ src/layout/components/HeaderLayout/index.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="grid grid-cols-24 justify-between items-center min-h-16 w-full"
+  <div class="grid grid-cols-24 justify-between items-center min-h-13 w-full"
     :class="themeStore.isDark ? 'bg-#000' : ''">
     <div class="px-2.5 ">
       11
     </div>
-    <div class="px-2.5 col-start-22 flex-center">
-      <ThemeToggler class="hover:bg-#DBDBFF/50 rounded-50% p-1 w-8 h-8 shrink-0 mr-2.5" />
-      <LocaleSwitcher class="hover:bg-#DBDBFF/50 rounded-50% p-1 w-8 h-8 shrink-0 mr-2.5" />
-      <FullScreen class="hover:bg-#DBDBFF/50 rounded-50% p-1 w-8 h-8 shrink-0 mr-2.5" />
-      <Notification class="hover:bg-#DBDBFF/50 rounded-50% p-1 w-8 h-8 shrink-0 mr-2.5" />
+    <div class="px-2.5 col-start-23 flex-center">
+      <ThemeToggler class="hover:bg-#DBDBFF/50 rounded-50% p-1 w-8 h-8 shrink-0 mr-4.5" />
+      <LocaleSwitcher class="hover:bg-#DBDBFF/50 rounded-50% p-1 w-8 h-8 shrink-0 mr-4.5" />
+      <FullScreen class="hover:bg-#DBDBFF/50 rounded-50% p-1 w-8 h-8 shrink-0 mr-4.5" />
+      <Notification class="hover:bg-#DBDBFF/50 rounded-50% p-1 w-8 h-8 shrink-0 mr-4.5" />
       <UserDropdown
-        class="hover:bg-#DBDBFF/50 rounded-50% p-1 w-8 h-8 shrink-0  bg-red b-3 box-content b-solid border-#fff shadow" />
+        class="hover:bg-#DBDBFF/50 rounded-50% p-1 w-7 h-7 shrink-0  bg-red b-3 box-content b-solid border-#fff shadow" />
     </div>
   </div>
 </template>
